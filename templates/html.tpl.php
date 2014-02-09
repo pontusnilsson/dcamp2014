42d2d2361d06fcdc9019168d839486feb1a57b93
Moa Nandorf <moa.nandorf@gmail.com>
2014-02-09 14:24:32 +0100

    Add meta tag for responsiveness

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -63,6 +63,7 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php if(!theme_get_setting('theme_arnold_script_place_footer')) { print $scripts; } ?>
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
